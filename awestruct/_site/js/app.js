55a7ed76635e739966d75309c69950f8744d3e1a
Bennett Roesch <meta@spyrosoft.com>
2016-03-23 11:56:37 -0700

    Complete the /efficiency.html page.

--- awestruct/_site/js/app.js
+++ awestruct/_site/js/app.js
@@ -1,1 +1,16 @@
 $( document ).foundation();
+
+$( '.show-quare-num-version' ).click( show_quare_num_version );
+
+function show_quare_num_version( click_event ) {
+	$( '.quare-num-version' ).addClass( 'display-none' );
+	try {
+		var version = click_event.toElement.value;
+	} catch( error ) { return; }
+	
+	if ( version == '1.0' ) {
+		$( '.quare-num-1-dot-0' ).removeClass( 'display-none' );
+	} else {
+		$( '.quare-num-2-dot-1' ).removeClass( 'display-none' );
+	}
+}
