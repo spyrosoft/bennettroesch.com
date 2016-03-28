4fdbc9351e32b9b77f5f11c8fff1f1648791ad63
Bennett Roesch <meta@spyrosoft.com>
2016-03-28 08:39:31 -0700

    Implement new standard init.lisp conventions.

--- init.lisp
+++ init.lisp
@@ -9,20 +9,21 @@
 ;; Needed if you set :error-template-directory in the easy-acceptor
 (setf hunchentoot::*show-lisp-errors-p* t)
 
-(defvar *hunchentoot-directory* (pathname (directory-namestring #.(or *compile-file-pathname* *load-truename*))))
+(defvar *hunchentoot-directory*
+  (pathname (directory-namestring #.(or *compile-file-pathname* *load-truename*))))
 
-(defvar *web-root-directory* (merge-pathnames "awestruct/_site/" *hunchentoot-directory*))
-
-(load "private/credentials.lisp")
+(defvar *web-root-directory*
+  (merge-pathnames "awestruct/_site/" *hunchentoot-directory*))
 
 (defvar bennettroesch-server
   (make-instance 'hunchentoot:easy-acceptor
                  :document-root *web-root-directory*
                  :error-template-directory (merge-pathnames "error-templates/" *web-root-directory*)
-                 :access-log-destination "logs/access.log"
-                 :message-log-destination "logs/error.log"
+                 :access-log-destination (merge-pathnames "logs/access.log" *hunchentoot-directory*)
+								 :message-log-destination (merge-pathnames "logs/message.log" *hunchentoot-directory*)
                  :port 8081))
 
+(load "private/credentials.lisp")
 (load "routes.lisp")
 
 (hunchentoot:start bennettroesch-server)
